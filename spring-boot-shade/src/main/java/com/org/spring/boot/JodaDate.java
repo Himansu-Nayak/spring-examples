0052592511e6bf96c5adbe241e43a628b9925f8e
Himansu Nayak H <himansu.h.nayak@ericsson.com>
2017-02-20 13:34:54 +0000

    maven-shade-plugin for spring boot

---
+++ spring-boot-shade/src/main/java/com/org/spring/boot/JodaDate.java
@@ -0,0 +1,11 @@
+package com.org.spring.boot;
+
+import org.joda.time.LocalDate;
+
+public class JodaDate {
+
+    public static void sampleJodaDate() {
+        System.out.println(new LocalDate().toString());
+    }
+
+}
---
+++ spring-boot-shade/src/main/java/com/org/spring/boot/Utility.java
@@ -0,0 +1,13 @@
+package com.org.spring.boot;
+
+/**
+ * Created by ehimnay on 20/02/2017.
+ */
+public class Utility {
+
+    public static void main(String... args) {
+        JodaDate.sampleJodaDate();
+        MultiMap.sampleMultiMap();
+    }
+
+}
